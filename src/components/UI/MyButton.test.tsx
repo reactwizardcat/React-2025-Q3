ea4a67c714484e3a872402bc799a2721fcf936a9
reactwizardcat <frozenghostty@gmail.com>
2025-07-18 15:12:28 +0300

    feat: add button test

--- src/components/UI/MyButton.test.tsx
+++ src/components/UI/MyButton.test.tsx
@@ -1,10 +1,36 @@
 import MyButton from './MyButton';
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/vitest';
+import userEvent from '@testing-library/user-event';
 
-test('button visubility', () => {
+describe('button component tests', () => {
   const text = 'Click Me';
-  render(<MyButton>{text}</MyButton>);
 
-  expect(screen.getByText(text)).toBeVisible();
+  it('renders the button with the correct text', () => {
+    render(<MyButton>{text}</MyButton>);
+    expect(screen.getByText(text)).toBeVisible();
+  });
+
+  it('should apply additional className', () => {
+    const testClass = 'test-class';
+    render(<MyButton className={testClass}>{text}</MyButton>);
+    expect(screen.getByRole('button')).toHaveClass(testClass);
+  });
+
+  it('calls the click handler when clicked', async () => {
+    const handleClick = vi.fn();
+    render(<MyButton callback={handleClick}>{text}</MyButton>);
+    await userEvent.click(screen.getByRole('button'));
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+
+  it('should not call callback when disabled', async () => {
+    const handleClick = vi.fn();
+    render(
+      <MyButton disabled callback={handleClick}>
+        {text}
+      </MyButton>
+    );
+    await userEvent.click(screen.getByRole('button'));
+    expect(handleClick).not.toHaveBeenCalled();
+  });
 });
--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
     coverage: {
       provider: 'v8',
       include: ['src/**/*.{js,jsx,ts,tsx}'],
